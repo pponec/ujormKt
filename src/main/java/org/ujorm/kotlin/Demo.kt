40ccd69463539b2dead5891f6902190138ca00d2
Pavel Ponec <pponec@gmail.com>
2021-07-04 22:19:40 +0200

    jUnit Test

--- src/main/java/org/ujorm/kotlin/Demo.kt
+++ src/main/java/org/ujorm/kotlin/Demo.kt
@@ -6,15 +6,16 @@ import kotlin.reflect.KClass
 /** Sample of usage */
 fun main(args: Array<String>) {
     val _user = ModelProvier.user
-    val crn1 = _user.name EQ "Xaver"
+    val crn1 = _user.name EQ "Pavel"
     val crn2 = _user.id GT 1
     val crn3 = _user.id LT 99
     val crn4 = crn1 OR (crn2 AND crn3)
     //val crn5 = crn1.not() OR (crn2 AND crn3) // TODO
-    assert(crn1() == "name EQ \"Xaver\"")
+    assert(crn1() == "name EQ \"Pavel\"")
+    assert(crn2() == "id GT 1")
     assert(crn2.toString() == "User(id GT 1)")
 
-    val user = User(11, "Pavel", LocalDate.now(), null)
+    val user = User(11, "Xaver", LocalDate.now(), null)
     val isValid : Boolean = crn4.eval(user)
     val userName : String = _user.name.of(user)
     val userId : Int = _user.id.of(user)
---
+++ src/test/java/org/ujorm/kotlin/DemoKtTest.kt
@@ -0,0 +1,13 @@
+package org.ujorm.kotlin
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*
+import org.ujorm.kotlin.main as main1
+
+internal class DemoKtTest {
+
+    @Test
+    fun mainTest() {
+        main1(arrayOf())
+    }
+}
