7a1ea2a0868c4f6bf685728edf53afe1c102056c
pponec <pponec@gmail.com>
2022-09-02 22:17:49 +0200

    Composed Properties

--- src/main/java/org/ujorm/kotlin/model/UjormComposed.kt
+++ src/main/java/org/ujorm/kotlin/model/UjormComposed.kt
@@ -38,38 +38,33 @@ abstract class EntityComposedModel<D : Any, V : Any> : PropertyImpl<D, V> {
     }
 }
 
-class ComposedProperty<D : Any, M : Any, V : Any> : PropertyNullable<D, V>{
-    private val primaryProperty: PropertyNullable<D, M>
-    private val secondaryProperty: PropertyNullable<M, V>
-    private val metaData: PropertyMetadata<D, V>
+class ComposedPropertyMetadata<D : Any, M : Any, V : Any>(
+    val primaryProperty: PropertyNullable<D, M>,
+    val secondaryProperty: PropertyNullable<M, V>
+) : PropertyMetadata<D, V> {
 
-    constructor(
-        primaryProperty: PropertyNullable<D, M>,
-        secondaryProperty: PropertyNullable<M, V>,
-        metaData: PropertyMetadata<D, V>
-    ) {
-        this.primaryProperty = primaryProperty
-        this.secondaryProperty = secondaryProperty
-        this.metaData = PropertyMetadataImpl(
-                    index = primaryProperty.data().index,
-                    name = "${primaryProperty.data().name}.${secondaryProperty.data().name}",
-                    entityClass = primaryProperty.data().entityClass,
-                    valueClass = secondaryProperty.data().valueClass,
-                    readOnly = primaryProperty.data().readOnly || secondaryProperty.data().readOnly,
-                    nullable = primaryProperty.data().nullable || secondaryProperty.data().nullable,
-            )
-    }
+    override val index: UByte get() = primaryProperty.data().index
+    override val name: String get() = "${primaryProperty.data().name}.${secondaryProperty.data().name}"
+    override val entityClass: KClass<D> get() = primaryProperty.data().entityClass
+    override val valueClass: KClass<V> get() = secondaryProperty.data().valueClass
+    override val readOnly = primaryProperty.data().readOnly || secondaryProperty.data().readOnly
+    override val nullable = primaryProperty.data().nullable || secondaryProperty.data().nullable
+}
+
+class ComposedProperty<D : Any, M : Any, V : Any>(
+    private val metaData: ComposedPropertyMetadata<D, M, V>
+    ) : PropertyNullable<D, V>{
 
     override fun data() = this.metaData
 
     override fun get(entity: D): V? {
-        val entity2 = primaryProperty[entity]
-        return if (entity2 != null) secondaryProperty[entity2] else null;
+        val entity2 = metaData.primaryProperty[entity]
+        return if (entity2 != null) metaData.secondaryProperty[entity2] else null;
     }
 
     override fun set(entity: D, value: V?) {
-        val entity2 = primaryProperty[entity]
-            ?: throw IllegalArgumentException("Value of property {$primaryProperty} is null")
-        secondaryProperty.set(entity2, value)
+        val entity2 = metaData.primaryProperty[entity]
+            ?: throw IllegalArgumentException("Value of property {$metaData.primaryProperty} is null")
+        metaData.secondaryProperty.set(entity2, value)
     }
 }
