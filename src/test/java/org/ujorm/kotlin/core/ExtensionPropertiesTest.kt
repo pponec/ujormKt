f93190d8850343d433395e2bdb480228129f7e1f
pponec <pponec@gmail.com>
2022-09-28 18:39:10 +0200

    Code cleaning

--- src/test/java/org/ujorm/kotlin/core/ExtensionPropertiesTest.kt
+++ src/test/java/org/ujorm/kotlin/core/ExtensionPropertiesTest.kt
@@ -2,24 +2,17 @@ package org.ujorm.kotlin.core
 
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-import kotlin.reflect.full.memberExtensionProperties
 import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.*
-
-
-
-class DummyClass{
-}
-
-val DummyClass.name get() = "John"
-val DummyClass.date get() = LocalDate.parse("2022-09-28")
-
+import kotlin.reflect.full.*
 
 class ExtensionPropertiesTest {
+    class DummyClass{}
+    val DummyClass.name get() = "John"
+    val DummyClass.date get() = LocalDate.parse("2022-09-28")
 
     @Test
     fun testExtensionProperties() {
-
         val dummyClass = DummyClass()
         expect(dummyClass.name).toEqual("John")
         expect(dummyClass.date.toString()).toEqual("2022-09-28")
@@ -29,6 +22,48 @@ class ExtensionPropertiesTest {
             .map { it.name }
             .toList()
 
-        expect(propertyNames).toHaveSize(2)
+        expect(propertyNames).toHaveSize(2) // Real size is zero (?)
+    }
+
+    @Test
+    fun testExtensionProperties2() {
+
+        val dummyClass = DummyClass()
+        expect(dummyClass.name).toEqual("John")
+        expect(dummyClass.date.toString()).toEqual("2022-09-28")
+
+        val propertyNames1 = DummyClass::class.staticProperties
+            .stream()
+            .map { it.name }
+            .toList()
+
+        val propertyNames2 = DummyClass::class.memberProperties
+            .stream()
+            .map { it.name }
+            .toList()
+
+        val propertyNames3 = DummyClass::class.memberExtensionProperties
+            .stream()
+            .map { it.name }
+            .toList()
+
+        val propertyNames4 = DummyClass::class.declaredMemberProperties
+            .stream()
+            .map { it.name }
+            .toList()
+
+        val propertyNames5 = DummyClass::class.declaredMemberExtensionProperties
+            .stream()
+            .map { it.name }
+            .toList()
+
+        val all = mutableListOf<String>()
+        all += propertyNames1
+        all += propertyNames2
+        all += propertyNames3
+        all += propertyNames4
+        all += propertyNames5
+
+        expect(all.size).notToEqual(0)
     }
 }
