4c335147a5fa4e71787522480106ee43830b2f6e
pponec <pponec@gmail.com>
2022-09-29 20:46:36 +0200

    ExtensionPropertiesTest

--- src/test/java/org/ujorm/kotlin/core/ExtensionPropertiesTest.kt
+++ src/test/java/org/ujorm/kotlin/core/ExtensionPropertiesTest.kt
@@ -17,53 +17,10 @@ class ExtensionPropertiesTest {
         expect(dummyClass.name).toEqual("John")
         expect(dummyClass.date.toString()).toEqual("2022-09-28")
 
-        val propertyNames = DummyClass::class.memberExtensionProperties
-            .stream()
+        val propertyNames = ExtensionPropertiesTest::class.memberExtensionProperties
             .map { it.name }
             .toList()
 
         expect(propertyNames).toHaveSize(2) // Real size is zero (?)
-    }
-
-    @Test
-    fun testExtensionProperties2() {
-
-        val dummyClass = DummyClass()
-        expect(dummyClass.name).toEqual("John")
-        expect(dummyClass.date.toString()).toEqual("2022-09-28")
-
-        val propertyNames1 = DummyClass::class.staticProperties
-            .stream()
-            .map { it.name }
-            .toList()
-
-        val propertyNames2 = DummyClass::class.memberProperties
-            .stream()
-            .map { it.name }
-            .toList()
-
-        val propertyNames3 = DummyClass::class.memberExtensionProperties
-            .stream()
-            .map { it.name }
-            .toList()
-
-        val propertyNames4 = DummyClass::class.declaredMemberProperties
-            .stream()
-            .map { it.name }
-            .toList()
-
-        val propertyNames5 = DummyClass::class.declaredMemberExtensionProperties
-            .stream()
-            .map { it.name }
-            .toList()
-
-        val all = mutableListOf<String>()
-        all += propertyNames1
-        all += propertyNames2
-        all += propertyNames3
-        all += propertyNames4
-        all += propertyNames5
-
-        expect(all.size).notToEqual(0)
     }
 }
