60d18e6d171bcf66ac866a0e639bb3a898e98c83
pponec <pponec@gmail.com>
2022-10-05 22:11:47 +0200

    Reflections

--- src/main/java/org/ujorm/kotlin/core/Reflections.kt
+++ src/main/java/org/ujorm/kotlin/core/Reflections.kt
@@ -2,25 +2,34 @@ package org.ujorm.kotlin.core
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.reflect.Modifier
 import java.util.stream.Stream
 import kotlin.reflect.KClass
 
 /** Source : https://www.baeldung.com/java-find-all-classes-in-package#1-system-class-loader */
 class Reflections {
 
-    fun findMemberExtensionObjectOfPackage(packageClass: KClass<*>): Stream<EntityModel<*>> {
+    fun findMemberExtensionObjectOfPackage(packageClass: KClass<*>, provider : Any): Stream<EntityModel<*>> {
         return findAllClassesOfPackage(packageClass)
             .filter{ it.simpleName.endsWith("Kt")}
-            .map { getObjects(it) }
+            .map { getObjects(it, provider) }
             .flatMap { it }
     }
 
-    fun getObjects(clazz : Class<*>) : Stream<EntityModel<*>> {
-        val domain = clazz.getDeclaredConstructor().newInstance()
+    fun getObjects(clazz : Class<*>, provider : Any) : Stream<EntityModel<*>> {
         return clazz.methods
-            .filter { it.parameterCount == 0 }
-            .filter { it.returnType.isAssignableFrom(EntityModel::class.java) }
-            .map { it.invoke(domain) }
+            .filter {
+                Modifier.isStatic(it.getModifiers())
+            }
+            .filter {
+                it.parameterCount == 1
+            }
+            .filter {
+                it.returnType.isAssignableFrom(EntityModel::class.java)
+            }
+            .map {
+                it.invoke(provider)
+            }
             .stream() as Stream<EntityModel<*>>
     }
 
--- src/test/java/org/ujorm/kotlin/core/ReflectionsTest.kt
+++ src/test/java/org/ujorm/kotlin/core/ReflectionsTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.*
 import org.ujorm.kotlin.core.entity.Departments
+import org.ujorm.kotlin.core.entity.EntityProvider
 
 internal class ReflectionsTest {
 
@@ -17,5 +18,28 @@ internal class ReflectionsTest {
             .toList()
 
         expect(classes).toHaveSize(2)
+    }
+
+    /**
+    java.lang.NoSuchMethodException: org.ujorm.kotlin.core.entity.DepartmentKt.<init>()
+
+    at java.base/java.lang.Class.getConstructor0(Class.java:3585)
+    at java.base/java.lang.Class.getDeclaredConstructor(Class.java:2754)
+    at org.ujorm.kotlin.core.Reflections.getObjects(Reflections.kt:19)
+
+    https://stackoverflow.com/questions/48635210/how-to-obtain-properties-or-function-declared-in-kotlin-extensions-by-java-refle
+    https://stackoverflow.com/questions/28294509/accessing-kotlin-extension-functions-from-java
+
+     */
+    @Test
+    fun findMemberExtensionObjectOfPackage() {
+        val provider = EntityProvider
+        val instance = Reflections()
+
+
+        val objects = instance.findMemberExtensionObjectOfPackage(provider::class, provider)
+            .toList()
+
+        expect(objects).toHaveSize(2)
     }
 }
