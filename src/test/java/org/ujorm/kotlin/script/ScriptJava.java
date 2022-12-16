34ef5440ea34799b66c6a12ff4552216bbcc0353
pponec <pponec@gmail.com>
2022-12-16 20:14:30 +0100

    Property.level

---
+++ src/test/java/org/ujorm/kotlin/script/ScriptJava.java
@@ -0,0 +1,50 @@
+package org.ujorm.kotlin.script;
+
+import org.junit.jupiter.api.Test;
+
+import javax.script.*;
+import ch.tutteli.atrium.api.fluent.en_GB.*;
+import ch.tutteli.atrium.api.verbs.*;
+import org.junit.jupiter.api.Test;
+import javax.script.ScriptEngineManager;
+import java.io.File;
+
+import static ch.tutteli.atrium.api.verbs.ExpectKt.expect;
+
+public class ScriptJava {
+
+    @Test
+    public void test2() throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("nashorn");
+        // create File object
+        File f = new File("test.txt");
+        // expose File object as a global variable to the engine
+        engine.put("file", f);
+        // evaluate JavaScript code and access the variable
+        engine.eval("print(file.getAbsolutePath())");
+
+        System.out.println("....");
+    }
+
+    @Test
+    public void testEval() throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("nashorn");
+        engine.put("data", new org.ujorm.kotlin.script.Data());
+
+
+        // evaluate JavaScript code
+        var result = engine.eval("data.text");
+
+        System.out.println(result);
+        assert "X".equals(String.valueOf(result)): "error";
+        // ch.tutteli.atrium.api.verbs.ExpectKt.expect(result).toEqual(7)
+    }
+
+    class Data {
+        public int i = 10;
+        public int j = 20;
+        public String text = "TEXT";
+    }
+}
---
+++ src/test/java/org/ujorm/kotlin/script/ScriptKt.kt
@@ -0,0 +1,53 @@
+package org.ujorm.kotlin.script
+
+import ch.tutteli.atrium.api.fluent.en_GB.toEqual
+import ch.tutteli.atrium.api.verbs.expect
+import org.junit.jupiter.api.Test
+import java.io.File
+import javax.script.ScriptEngineManager
+
+internal class ScriptTests {
+
+    @Test
+    internal fun runScript1() {
+        val f = File("test22.txt")
+
+        val engine = ScriptEngineManager().getEngineByExtension("kts")!!.apply {
+            put("f", f)
+        }
+
+        var e1 = "${f.getAbsolutePath()}" as Any?
+        var e2 = engine.eval("\"${f.getAbsolutePath()}\"")
+
+        expect(e1).toEqual(e2)
+    }
+
+    @Test
+    internal fun runScript2() {
+
+        val data = Data(10, 20, Data())
+
+        val engine = ScriptEngineManager().getEngineByExtension("kts")!!.apply {
+            put("data", data)
+        }
+
+        var e1 = "${data.i}" as Any?
+        var e2 = engine.eval("\"${data.i}\"")
+
+        expect(e1).toEqual(e2)
+    }
+
+}
+
+class Data (
+    val i: Int = 400,
+    val j: Int = 555,
+    val data: Data? = null,
+) {
+    override fun toString(): String {
+        return "Data(i=$i, j=$j)"
+    }
+}
+
+
+
