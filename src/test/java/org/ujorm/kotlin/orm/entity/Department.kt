515ec6f30e600ade5b0bb9c71651afd8ee43e2b8
pponec <pponec@gmail.com>
2023-02-20 22:05:43 +0100

    The chained properties API is removed due a problem with getting non-null values.

--- src/test/java/org/ujorm/kotlin/orm/entity/Department.kt
+++ src/test/java/org/ujorm/kotlin/orm/entity/Department.kt
@@ -2,8 +2,8 @@ package org.ujorm.kotlin.orm.entity
 
 import org.ujorm.kotlin.anotation.Entity
 import org.ujorm.kotlin.anotation.OneToMany
+import org.ujorm.kotlin.core.entity.Entities
 import org.ujorm.kotlin.core.impl.EntityModel
-import org.ujorm.kotlin.coreComposed.DomainEntityModel
 import java.time.LocalDate
 
 /** A Department entity */
@@ -17,25 +17,14 @@ interface Department {
 }
 
 /** Model of the entity can be a generated class in the feature */
-open class _Departments : EntityModel<Department>(Department::class) {
+open class Departments : EntityModel<Department>(Department::class) {
     val id = property { it.id }
     val name = property { it.name }
     val created = property { Department::created } // Alternative notation
     val members = propertyList { Employee::department } // Notation for a Relation 1:M
 }
 
-/** Model of the entity can be a generated class in the feature */
-open class Departments<D : Any> : DomainEntityModel<D, Department>() {
-    override val core: _Departments = _Departments().close()
-
-    // --- Entity Properties ---
-
-    val id get() = propertyModel(core.id)
-    val name get() = propertyModel(core.name)
-    val created get() = propertyModel(core.created)
-}
-
 /** Initialize, register and close the entity model. */
 val Database.departments by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
-    Departments<Department>().close() as Departments<Department>
+    Entities.add(Departments().close<Departments>())
 }
