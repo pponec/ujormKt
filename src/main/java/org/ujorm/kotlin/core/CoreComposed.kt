3a97afebca848681149fe59f62ad49ea882a35d9
pponec <pponec@gmail.com>
2023-02-20 20:31:31 +0100

    Code cleaning

--- src/main/java/org/ujorm/kotlin/core/CoreComposed.kt
+++ src/main/java/org/ujorm/kotlin/core/CoreComposed.kt
@@ -82,8 +82,17 @@ abstract class DomainEntityModel<D : Any, V : Any> : PropertyNullable<D, V> {
         }
     }
 
+    /** Build the new Property */
+    protected fun <M : Any, V : Any, R : PropertyNullable<D, V>> property(property : PropertyNullable<M, V>) : R {
+        if (composedProperty != null) {
+            TODO()
+        } else {
+            return property as R
+        }
+    }
+
     /** Return the plain the new Property */
-    protected fun <N : Any> property(property : PropertyNullable<V, N>) : PropertyNullable<D, N> {
+    protected fun <N : Any> propertyRaw(property : PropertyNullable<V, N>) : PropertyNullable<D, N> {
         return if (composedProperty != null) {
             composedProperty + property
         } else {
